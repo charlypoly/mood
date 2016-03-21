fc6f8f7be07174f29a7b5771d07148d2fe66638f
Charly POLY <cpoly55@gmail.com>
2016-03-21 17:42:59 +0100

    rails init for POC

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mood::Application.config.secret_key_base = '88e15c0cffe303157f2fc2a06e3112e081230b4b2d7089cf87798533800436c2b8b735e37702bf17aad95ec88df233270f8836f1217ca12521d0e195dcb8385a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Mood::Application.config.session_store :cookie_store, key: '_mood_session'
